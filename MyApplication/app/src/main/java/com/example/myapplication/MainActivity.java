9b0c9eee20f5851f4bf78bf9b1ae6519e3fa933d
omPatel1718 <67968273+omPatel1718@users.noreply.github.com>
2022-08-25 19:57:30 -0500

    added arc trig functions, e^x, and color changes.

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,103 +15,181 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void findSum(View v){
-        EditText number1ET = findViewById(R.id.num1ET);
-        EditText number2ET = findViewById(R.id.num2ET);
-        TextView numberSumTV = findViewById(R.id.resultTV);
+        EditText number1ET;
+        EditText number2ET;
+        TextView numberTotalTV = findViewById(R.id.resultTV);
+        try {
+            number1ET = findViewById(R.id.num1ET);
+            number2ET = findViewById(R.id.num2ET);
+        }catch (NumberFormatException e){
+            numberTotalTV.setText("Please only input numbers.");
+            return;
+        }
 
         int num1 =Integer.parseInt(number1ET.getText().toString());
         int num2 =Integer.parseInt(number2ET.getText().toString());
         int sum = num1 + num2;
 
-        numberSumTV.setText("" + sum);
+        numberTotalTV.setText("" + sum);
+        number1ET.setText("");
+        number2ET.setText("");
     }
 
     public void findDifference(View v){
-        EditText number1ET = findViewById(R.id.num1ET);
-        EditText number2ET = findViewById(R.id.num2ET);
-        TextView numberDifferenceTV = findViewById(R.id.resultTV);
+        EditText number1ET;
+        EditText number2ET;
+        TextView numberTotalTV = findViewById(R.id.resultTV);
+        try {
+            number1ET = findViewById(R.id.num1ET);
+            number2ET = findViewById(R.id.num2ET);
+        }catch (NumberFormatException e){
+            numberTotalTV.setText("Please only input numbers.");
+            return;
+        }
 
         int num1 =Integer.parseInt(number1ET.getText().toString());
         int num2 =Integer.parseInt(number2ET.getText().toString());
         int difference = num1 - num2;
 
-        numberDifferenceTV.setText("" + difference);
+        numberTotalTV.setText("" + difference);
+        number1ET.setText("");
+        number2ET.setText("");
     }
 
     public void findMulti(View v){
-        EditText number1ET = findViewById(R.id.num1ET);
-        EditText number2ET = findViewById(R.id.num2ET);
+        EditText number1ET;
+        EditText number2ET;
         TextView numberTotalTV = findViewById(R.id.resultTV);
+        try {
+            number1ET = findViewById(R.id.num1ET);
+            number2ET = findViewById(R.id.num2ET);
+        }catch (NumberFormatException e){
+            numberTotalTV.setText("Please only input numbers.");
+            return;
+        }
 
         int num1 =Integer.parseInt(number1ET.getText().toString());
         int num2 =Integer.parseInt(number2ET.getText().toString());
         int total = num1 * num2;
 
         numberTotalTV.setText("" + total);
+        number1ET.setText("");
+        number2ET.setText("");
     }
 
     public void findDivide(View v){
-        EditText number1ET = findViewById(R.id.num1ET);
-        EditText number2ET = findViewById(R.id.num2ET);
+        EditText number1ET;
+        EditText number2ET;
         TextView numberTotalTV = findViewById(R.id.resultTV);
+        try {
+            number1ET = findViewById(R.id.num1ET);
+            number2ET = findViewById(R.id.num2ET);
+        }catch (NumberFormatException e){
+            numberTotalTV.setText("Please only input numbers.");
+            return;
+        }
 
         int num1 =Integer.parseInt(number1ET.getText().toString());
         int num2 =Integer.parseInt(number2ET.getText().toString());
         int total = num1 / num2;
 
         numberTotalTV.setText("" + total);
+        number1ET.setText("");
+        number2ET.setText("");
     }
 
     public void findMod(View v){
-        EditText number1ET = findViewById(R.id.num1ET);
-        EditText number2ET = findViewById(R.id.num2ET);
+        EditText number1ET;
+        EditText number2ET;
         TextView numberTotalTV = findViewById(R.id.resultTV);
+        try {
+            number1ET = findViewById(R.id.num1ET);
+            number2ET = findViewById(R.id.num2ET);
+        }catch (NumberFormatException e){
+            numberTotalTV.setText("Please only input numbers.");
+            return;
+        }
 
         int num1 =Integer.parseInt(number1ET.getText().toString());
         int num2 =Integer.parseInt(number2ET.getText().toString());
         int total = num1 % num2;
 
         numberTotalTV.setText("" + total);
+        number1ET.setText("");
+        number2ET.setText("");
     }
 
     public void findPower(View v){
-        EditText number1ET = findViewById(R.id.num1ET);
-        EditText number2ET = findViewById(R.id.num2ET);
+        EditText number1ET;
+        EditText number2ET;
         TextView numberTotalTV = findViewById(R.id.resultTV);
+        try {
+            number1ET = findViewById(R.id.num1ET);
+            number2ET = findViewById(R.id.num2ET);
+        }catch (NumberFormatException e){
+            numberTotalTV.setText("Please only input numbers.");
+            return;
+        }
 
         int num1 =Integer.parseInt(number1ET.getText().toString());
         int num2 =Integer.parseInt(number2ET.getText().toString());
         int total = num1 ^ num2;
 
         numberTotalTV.setText("" + total);
+        number1ET.setText("");
+        number2ET.setText("");
     }
 
     public void findRoot(View v){
-        EditText number1ET = findViewById(R.id.num1ET);
-        EditText number2ET = findViewById(R.id.num2ET);
+        EditText number1ET;
+        EditText number2ET;
         TextView numberTotalTV = findViewById(R.id.resultTV);
+        try {
+            number1ET = findViewById(R.id.num1ET);
+            number2ET = findViewById(R.id.num2ET);
+        }catch (NumberFormatException e){
+            numberTotalTV.setText("Please only input numbers.");
+            return;
+        }
 
         int num1 =Integer.parseInt(number1ET.getText().toString());
         int num2 =Integer.parseInt(number2ET.getText().toString());
         double total = Math.pow(num1, 1/num2);
 
         numberTotalTV.setText("" + total);
+        number1ET.setText("");
+        number2ET.setText("");
     }
     public void findLog(View v){
-        EditText number1ET = findViewById(R.id.num1ET);
-        EditText number2ET = findViewById(R.id.num2ET);
+        EditText number1ET;
+        EditText number2ET;
         TextView numberTotalTV = findViewById(R.id.resultTV);
+        try {
+            number1ET = findViewById(R.id.num1ET);
+            number2ET = findViewById(R.id.num2ET);
+        }catch (NumberFormatException e){
+            numberTotalTV.setText("Please only input numbers.");
+            return;
+        }
 
         int num1 =Integer.parseInt(number1ET.getText().toString());
         int num2 =Integer.parseInt(number2ET.getText().toString());
         double total = Math.log(num1)/Math.log(num2);
 
         numberTotalTV.setText("" + total);
+        number1ET.setText("");
+        number2ET.setText("");
     }
 
     public void findFactorial(View v){
-        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number1ET;
         TextView numberTotalTV = findViewById(R.id.resultTV);
+        try {
+            number1ET = findViewById(R.id.num1ET);
+        }catch (NumberFormatException e){
+            numberTotalTV.setText("Please only input numbers.");
+            return;
+        }
 
         int num = Integer.parseInt(number1ET.getText().toString());
         int total = 1;
@@ -120,35 +198,137 @@ public class MainActivity extends AppCompatActivity {
         }
 
         numberTotalTV.setText("" + total);
+        number1ET.setText("");
     }
 
     public void findSine(View v){
-        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number1ET;
         TextView numberTotalTV = findViewById(R.id.resultTV);
+        try {
+            number1ET = findViewById(R.id.num1ET);
+        }catch (NumberFormatException e){
+            numberTotalTV.setText("Please only input numbers.");
+            return;
+        }
 
         int num = Integer.parseInt(number1ET.getText().toString());
         double total = Math.sin(num);
 
         numberTotalTV.setText("" + total);
+        number1ET.setText("");
     }
 
     public void findCosine(View v){
-        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number1ET;
         TextView numberTotalTV = findViewById(R.id.resultTV);
+        try {
+            number1ET = findViewById(R.id.num1ET);
+        }catch (NumberFormatException e){
+            numberTotalTV.setText("Please only input numbers.");
+            return;
+        }
 
         int num = Integer.parseInt(number1ET.getText().toString());
         double total = Math.cos(num);
 
         numberTotalTV.setText("" + total);
+        number1ET.setText("");
     }
 
     public void findTangent(View v){
-        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number1ET;
         TextView numberTotalTV = findViewById(R.id.resultTV);
+        try {
+            number1ET = findViewById(R.id.num1ET);
+        }catch (NumberFormatException e){
+            numberTotalTV.setText("Please only input numbers.");
+            return;
+        }
 
         int num = Integer.parseInt(number1ET.getText().toString());
         double total = Math.tan(num);
 
         numberTotalTV.setText("" + total);
+        number1ET.setText("");
+    }
+
+    public void findArcSine(View v){
+        EditText number1ET;
+        EditText number2ET;
+        TextView numberTotalTV = findViewById(R.id.resultTV);
+        try {
+            number1ET = findViewById(R.id.num1ET);
+            number2ET = findViewById(R.id.num2ET);
+        }catch (NumberFormatException e){
+            numberTotalTV.setText("Please only input numbers.");
+            return;
+        }
+
+        int num1 =Integer.parseInt(number1ET.getText().toString());
+        int num2 =Integer.parseInt(number2ET.getText().toString());
+        double total = Math.asin(num1/num2);
+
+        numberTotalTV.setText("" + total);
+        number1ET.setText("");
+        number2ET.setText("");
+    }
+
+    public void findArcCosine(View v){
+        EditText number1ET;
+        EditText number2ET;
+        TextView numberTotalTV = findViewById(R.id.resultTV);
+        try {
+            number1ET = findViewById(R.id.num1ET);
+            number2ET = findViewById(R.id.num2ET);
+        }catch (NumberFormatException e){
+            numberTotalTV.setText("Please only input numbers.");
+            return;
+        }
+
+        int num1 =Integer.parseInt(number1ET.getText().toString());
+        int num2 =Integer.parseInt(number2ET.getText().toString());
+        double total = Math.acos(num1/num2);
+
+        numberTotalTV.setText("" + total);
+        number1ET.setText("");
+        number2ET.setText("");
+    }
+
+    public void findArcTangent(View v){
+        EditText number1ET;
+        EditText number2ET;
+        TextView numberTotalTV = findViewById(R.id.resultTV);
+        try {
+            number1ET = findViewById(R.id.num1ET);
+            number2ET = findViewById(R.id.num2ET);
+        }catch (NumberFormatException e){
+            numberTotalTV.setText("Please only input numbers.");
+            return;
+        }
+
+        int num1 =Integer.parseInt(number1ET.getText().toString());
+        int num2 =Integer.parseInt(number2ET.getText().toString());
+        double total = Math.atan(num1/num2);
+
+        numberTotalTV.setText("" + total);
+        number1ET.setText("");
+        number2ET.setText("");
+    }
+
+    public void findEPower(View v){
+        EditText number1ET;
+        TextView numberTotalTV = findViewById(R.id.resultTV);
+        try {
+            number1ET = findViewById(R.id.num1ET);
+        }catch (NumberFormatException e){
+            numberTotalTV.setText("Please only input numbers.");
+            return;
+        }
+
+        int num = Integer.parseInt(number1ET.getText().toString());
+        double total = Math.pow(Math.E,num);
+
+        numberTotalTV.setText("" + total);
+        number1ET.setText("");
     }
 }
